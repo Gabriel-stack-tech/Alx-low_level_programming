a637d63fc40c87627890f7a8080ac2260e9feeed
shade <shadegabriel97@gmail.com>
2022-09-11 04:24:09 -0700

    commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Our name is oyinkansoye susan Adekunle */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
