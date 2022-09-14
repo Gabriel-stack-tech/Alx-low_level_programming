1f64bacf379d431dccde812dd2261a5cc3bb7717
shade <shadegabriel97@gmail.com>
2022-09-14 15:40:37 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * int is use for argument
+ * function to check for lowe case
+ * Return:0
+ */
+int islower(int)
+{
+	if(c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
+
