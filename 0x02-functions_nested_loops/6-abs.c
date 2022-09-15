e53e7ed0b883cf141d0d5fe51eca22cb1e45a1b5
shade <shadegabriel97@gmail.com>
2022-09-14 18:24:00 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main"
+#include <stdio.h>
+/**
+ * function that computers the absolute value
+ * argument of the function
+ * Return:0
+ */ 
+int -abs(int c)
+{
+	if ( c > o || c == 0)
+	{
+		return(1);
+	}
+	else
+		return(c * -1);
+}
+
