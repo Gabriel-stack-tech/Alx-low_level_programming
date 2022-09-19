d468f422cc9b7e1be596a18676153cdd54aade32
shade <shadegabriel97@gmail.com>
2022-09-19 16:59:43 -0700

    2-swap.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen -returns the length of a string
+ * @:string
+ * Return:lenth as integer
+ */
+
+int _strlen(char *s)
+{
+	int j = 0;
+
+	while (*(s + j)! = '\0'; j++)
+
+		return (j);
+}
+
