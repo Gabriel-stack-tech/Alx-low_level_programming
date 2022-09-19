da59a341fce196aef9a7b33477424a1e8fd2c5d5
shade <shadegabriel97@gmail.com>
2022-09-19 15:36:48 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* reset_to_98 update a pointers reference valve to 98* @n: the number to be update
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+
+' project is all about '
+ 
+  0x05-pointers, arrays and strings
+
