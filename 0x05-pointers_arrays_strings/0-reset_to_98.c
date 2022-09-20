3105c177cf2ca3be4fe1697866c6a5b4670d9ac3
shade <shadegabriel97@gmail.com>
2022-09-20 07:30:48 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-* reset_to_98 update a pointers reference valve to 98* @n: the number to be update
+* reset_to_98 update a pointers reference valve to 98
+* @n: the number to be update
 */
 
 void reset_to_98(int *n)
