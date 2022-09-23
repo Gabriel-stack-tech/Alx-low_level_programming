295998856d759646c76f437be31d2279116c9a0f
shade <shadegabriel97@gmail.com>
2022-09-23 14:44:39 -0700

    0-iupper.c

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+0x04.c-more functions
+more nested loop
+
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_number(void);
+void more_number(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void printe_number(int n);
+
+
+#endif /" mAIN.H */
+
