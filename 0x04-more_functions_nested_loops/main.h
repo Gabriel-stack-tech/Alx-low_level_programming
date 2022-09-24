eb485b4d24d963f17559cdeb418fbbc69d0d0a2b
shade <shadegabriel97@gmail.com>
2022-09-24 12:28:27 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -11,8 +12,8 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void printe_number(int n);
+void print_number(int n);
 
 
-#endif /" mAIN.H */
+#endif /* MAIN_H */
 
